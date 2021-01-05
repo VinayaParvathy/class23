39fbbc0558e3d51a66a60495cbd1d18cd45e6407
Vinaya <vinayaparvathi@gmail>
2021-01-05 10:39:59 +0530

    Added Platform to the game

--- sketch.js
+++ sketch.js
@@ -16,7 +16,9 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+
+    platform=new Ground(180,200,600,300);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
